697122f9394f1916dad5514cc8cb44a2a56ec40c
joelsven <josv.svensson@gmail.com>
2019-04-27 12:29:56 +0200

    Add player

---
+++ Assets/Scripts/Keybindings.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+class Keybindings {
+    public static float MoveLeft => (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) ? 1f : 0f;
+    public static float MoveRight => (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) ? 1f : 0f;
+    public static float MoveUp => (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) ? 1f : 0f;
+    public static float MoveDown => (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) ? 1f : 0f;
+    public static bool Attack => Input.GetMouseButton(0);
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Player : MonoBehaviour {
+    public float maxSpeed = 8f;
+    public float acceleration = 25f;
+    public float deceleration = 15f;
+
+    private new Rigidbody2D rigidbody;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 inputVector = Vector3.zero;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        CalculateInput();
+    }
+
+    void FixedUpdate() {
+        CalculateDeceleration();
+        CalculateVelocity();
+    }
+
+    private void CalculateInput() {
+        inputVector = Vector2.zero;
+        inputVector.x -= Keybindings.MoveLeft;
+        inputVector.x += Keybindings.MoveRight;
+        inputVector.y += Keybindings.MoveUp;
+        inputVector.y -= Keybindings.MoveDown;
+    }
+
+    private void CalculateDeceleration() {
+        if (inputVector.x <=  0f && velocity.x > 0f) {
+            velocity.x -= deceleration * Time.deltaTime;
+            if (velocity.x < 0f) velocity.x = 0f;
+        }
+        if (inputVector.x >= 0f && velocity.x < 0f) {
+            velocity.x += deceleration * Time.deltaTime;
+            if (velocity.x > 0f) velocity.x = 0f;
+        }
+
+        if (inputVector.y <= 0f && velocity.y > 0f) {
+            velocity.y -= deceleration * Time.deltaTime;
+            if (velocity.y < 0f) velocity.y = 0f;
+        }
+        if (inputVector.y >= 0f && velocity.y < 0f) {
+            velocity.y += deceleration * Time.deltaTime;
+            if (velocity.y > 0f) velocity.y = 0f;
+        }
+    }
+
+    private void CalculateVelocity() {
+        velocity += inputVector.normalized * acceleration * Time.deltaTime;
+
+        if (velocity.magnitude > maxSpeed) {
+            velocity = velocity.normalized * maxSpeed;
+        }
+
+        rigidbody.velocity = velocity;
     }
 }
