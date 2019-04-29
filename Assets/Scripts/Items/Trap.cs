9128178623aa28c637f9dfd4c7b73490d4f8ae8e
Bentebent <henriksson.tim@gmail.com>
2019-04-29 15:04:30 +0200

    Removed friendly fire from enemies

---
+++ Assets/Scripts/Items/Trap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Trap : Item
+{
+    public override void ApplyEffect(GameObject owner)
+    {
+
+    }
+}
