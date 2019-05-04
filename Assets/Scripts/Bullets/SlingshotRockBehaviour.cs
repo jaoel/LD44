5b88c40ed9d733709b41b4399fb008d0b46cf835
joelsven <josv.svensson@gmail.com>
2019-05-04 14:46:34 +0200

    Add projectile to slingshot

---
+++ Assets/Scripts/Bullets/SlingshotRockBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SlingshotRockBehaviour : BulletBehaviour {
+    public Transform visualTransform;
+    private float rot = 0f;
+
+    private void Start() {
+        rot = Random.Range(0f, 360f);
+    }
+
+    public override void UpdateBullet(BulletInstance bullet) {
+        base.UpdateBullet(bullet);
+        rot += 300f * Time.deltaTime;
+        visualTransform.rotation = Quaternion.Euler(0f, 0f, rot);
+    }
+}
