d80d66264a645459c587627ade19db53557c19e1
joelsven <josv.svensson@gmail.com>
2019-05-07 23:54:33 +0200

    WIP vampire

--- Assets/Scripts/Enemies/EnemyContainer.cs
+++ Assets/Scripts/Enemies/EnemyContainer.cs
@@ -6,4 +6,5 @@ public class EnemyContainer : ScriptableObject
     public GameObject basicZombie;
     public GameObject shootingZombie;
     public GameObject shotgunZombie;
+    public GameObject vampire;
 }
---
+++ Assets/Scripts/Enemies/VampireEnemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class VampireEnemy : Enemy {
+    public GameObject vampireVisuals;
+    public GameObject batVisuals;
+
+    protected override void FixedUpdate() {
+        base.FixedUpdate();
+    }
+
+    protected override bool PlayAttackAnimation() {
+        return Vector2.Distance(_target, transform.position) < _stoppingDistance;
+    }
+} 
