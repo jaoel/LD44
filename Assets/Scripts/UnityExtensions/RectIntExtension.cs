cf0ba943544eebc82cde4f1b09fca65e16397608
Bentebent <henriksson.tim@gmail.com>
2019-05-09 14:46:29 +0200

    Refactored extension methods to use new standard

--- Assets/Scripts/UnityExtensions/RectIntExtension.cs
+++ Assets/Scripts/UnityExtensions/RectIntExtension.cs
@@ -14,12 +14,14 @@ public static class RectIntExtension
     public static bool Contains(this RectInt a, Vector2 point)
     {
         if (a.xMin <= point.x && a.xMax >= point.x && a.yMin <= point.y && a.yMax >= point.y)
+        {
             return true;
-       
+        }
+
         return false;
     }
     public static int Area(this RectInt rect)
     {
         return rect.width * rect.height;
     }
-}  
+}
