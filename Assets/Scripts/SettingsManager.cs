4d9e563bb040fa13144cb0c65e78917002880db9
joelsven <josv.svensson@gmail.com>
2019-05-11 18:55:40 +0200

    Add settings to player prefs

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -1,41 +1,54 @@
 ﻿using UnityEngine;
 
+public static class PlayerPrefsStrings
+{
+    public static readonly string MusicVolume = "settings_music_volume";
+    public static readonly string SfxVolume = "settings_sfx_volume";
+    public static readonly string ScreenShakeScale = "settings_screen_shake_scale";
+}
+
 public class SettingsManager
 {
-    private float linearMusicVolume = 0f;
-    private float linearSFXVolume = 0f;
+    private static SettingsManager _instance;
+
+    private float _linearMusicVolume = 0f;
+    private float _linearSFXVolume = 0f;
+    private float _screenShakeScale;
 
     public float MusicVolume { get; private set; }
     public float SFXVolume { get; private set; }
-    public float ScreenShakeScale { get; set; }
+
+    public float ScreenShakeScale
+    {
+        get { return _screenShakeScale; }
+        set
+        {
+            _screenShakeScale = value;
+            PlayerPrefs.SetFloat(PlayerPrefsStrings.ScreenShakeScale, value);
+        }
+    }
 
     public float LinearMusicVolume
     {
-        get
-        {
-            return linearMusicVolume;
-        }
+        get { return _linearMusicVolume; }
         set
         {
-            linearMusicVolume = value;
+            _linearMusicVolume = value;
             MusicVolume = value * value;
+            PlayerPrefs.SetFloat(PlayerPrefsStrings.MusicVolume, value);
         }
     }
 
     public float LinearSFXVolume
     {
-        get
-        {
-            return linearSFXVolume;
-        }
+        get { return _linearSFXVolume; }
         set
         {
-            linearSFXVolume = value;
+            _linearSFXVolume = value;
             SFXVolume = value * value;
+            PlayerPrefs.SetFloat(PlayerPrefsStrings.SfxVolume, value);
         }
     }
-
-    private static SettingsManager _instance;
 
     public static SettingsManager Instance
     {
@@ -44,9 +57,9 @@ public class SettingsManager
             if (_instance == null)
             {
                 _instance = new SettingsManager();
-                _instance.LinearMusicVolume = 0.5f;
-                _instance.LinearSFXVolume = 0.5f;
-                _instance.ScreenShakeScale = 1.0f;
+                _instance.LinearMusicVolume = PlayerPrefs.GetFloat(PlayerPrefsStrings.MusicVolume, 0.5f);
+                _instance.LinearSFXVolume = PlayerPrefs.GetFloat(PlayerPrefsStrings.SfxVolume, 0.5f);
+                _instance.ScreenShakeScale = PlayerPrefs.GetFloat(PlayerPrefsStrings.ScreenShakeScale, 1.0f);
             }
 
             return _instance;
