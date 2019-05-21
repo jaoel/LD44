0fa0a8ccba1aa1fb7d2dbd88b7089ac96cdea079
Bentebent <henriksson.tim@gmail.com>
2019-05-21 11:09:28 +0200

    Added spawnable classes

---
+++ Assets/Scripts/Map/Spawnable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpawnableContainer))]
+public class SpawnableInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("Spawnable keyframe data"); 
+    }
+}
+
+[CreateAssetMenu]
+[Serializable]
+public class SpawnableContainer : ScriptableObject
+{
+    List<SpawnableKeyframe> keyframes;
+}
+
+
+[Serializable]
+public class SpawnableKeyframe
+{
+    public int keyframeIndex;
+    public List<Spawnable> spawnableObjects;
+}
+
+
+[Serializable]
+public class Spawnable
+{
+    public GameObject spawnablePrefab;
+    public List<float> densities;
+}
