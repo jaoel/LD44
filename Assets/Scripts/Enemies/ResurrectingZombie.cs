1da07bcbf55dd8b6ab5b9555741fc7bdc802dcb6
Bentebent <henriksson.tim@gmail.com>
2019-05-29 21:40:04 +0200

    Zombie tweaks

--- Assets/Scripts/Enemies/ResurrectingZombie.cs
+++ Assets/Scripts/Enemies/ResurrectingZombie.cs
@@ -8,6 +8,9 @@ public class ResurrectingZombie : ResurrectingEnemy
 
     [SerializeField]
     protected float _resurrectedMaxHealth;
+
+    [SerializeField]
+    protected SpriteRenderer _visual;
     
     protected override void Resurrection()
     {
@@ -15,6 +18,7 @@ public class ResurrectingZombie : ResurrectingEnemy
         _maxHealth = _resurrectedMaxHealth;
 
         _navigation.Initialize(_rigidbody, _maxSpeed, _acceleration * 2.0f);
+        _visual.color = Color.red;
 
         base.Resurrection();
     }
