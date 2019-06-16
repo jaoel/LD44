09d8f2feaaa4c5a722d5927af5801a263c383e7c
Jaoel <josv.svensson@gmail.com>
2019-06-16 16:04:13 +0200

    Add double height door support

---
+++ Assets/Scripts/AnimatedSpriteMask.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteMask), typeof(SpriteRenderer))]
+public class AnimatedSpriteMask : MonoBehaviour
+{
+    private SpriteMask _spriteMask;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteMask = GetComponent<SpriteMask>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        if(_spriteMask.sprite != _spriteRenderer.sprite)
+        {
+            _spriteMask.sprite = _spriteRenderer.sprite;
+            Debug.Log("HURR");
+        }
+    }
+}
