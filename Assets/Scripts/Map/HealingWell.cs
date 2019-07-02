84cbf08de2a300f8977367a63daaa1badd3edbb0
Bentebent <henriksson.tim@gmail.com>
2019-07-02 16:53:17 +0200

    Rest area healing well balancing

--- Assets/Scripts/Map/HealingWell.cs
+++ Assets/Scripts/Map/HealingWell.cs
@@ -11,11 +11,23 @@ public class HealingWell : InteractiveObject
     public int maxHealthDamage;
     public float tickRate;
 
+    private int _adjustedHeal;
+    private int _adjustedDamage;
+
     private float _timer;
+    private float _multiplierTimer = 0.0f;
 
     private void OnEnable()
     {
         _timer = 0.0f;
+        _multiplierTimer = 0.0f;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        _adjustedHeal = heal;
+        _adjustedDamage = maxHealthDamage;
+        base.OnTriggerEnter2D(collision);
     }
 
     protected override void OnTriggerStay2D(Collider2D collision)
@@ -24,14 +36,30 @@ public class HealingWell : InteractiveObject
         if (player != null)
         {
             _timer += Time.deltaTime;
+            _multiplierTimer += Time.deltaTime;
+
+            if (_multiplierTimer > 5.0f)
+            {
+                _adjustedHeal *= 2;
+                _adjustedDamage *= 2;
+                _multiplierTimer = 0.0f;
+                SoundManager.Instance.PlayPainSound();
+            }
 
             if (_timer >= tickRate && player.Health < player.MaxHealth)
             {
-                player.MaxHealth -= maxHealthDamage;
-                player.Health += heal;
+                player.MaxHealth -= _adjustedDamage;
+                player.Health += _adjustedHeal;
 
                 _timer = 0.0f;
             }
         }
+    }
+
+    protected override void OnTriggerExit2D(Collider2D collision)
+    {
+        _timer = 0.0f;
+        _multiplierTimer = 0.0f;
+        base.OnTriggerExit2D(collision);
     }
 }
